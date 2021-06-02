bc1656adaa854e3d6a060e2df15516dbe51bf592
AvneetKaur-ITP <85129606+AvneetKaur-ITP@users.noreply.github.com>
2021-06-02 22:43:25 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char op;
+    float n1, n2;
+
+    print("what do you want to print");
+    switch(op){
+ case '+':
+    float sum;
+    break;
+ case '-':
+    print ("bye");
+    break;
+ default:
+    print("i don't know");
+    }
+    return 0;
+}
